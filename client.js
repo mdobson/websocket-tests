98a2658ec24fc125bdac5257269c6e01598604a0
Matt Dobson <mdobson4@gmail.com>
2013-12-19 09:15:22 -0500

    added procfile.

--- client.js
+++ client.js
@@ -1,1 +1,11 @@
+var WebSocket = require('ws'),
+    ws = new WebSocket('ws://argosocktest.herokuapp.com/');
 
+ws.on('open', function() {
+  ws.send('Send some data to echo!');
+});
+
+
+ws.on('message', function(data, flags) {
+  console.log(arguments);
+});
